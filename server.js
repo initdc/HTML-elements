10870785a700db73932b6d9c8e9058ce56ff1416
initdc <initd@outlook.com>
2021-04-29 16:25:42 +0800

    [0.0.6] *add repo link

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
+var env = process.env.PORT
+
+if (env === undefined || env === ''){
+    var port = '8080'
+}else {
+    var port = env
+}
+console.log('check here: '+port)
+
 http.createServer(function (request, response) {
   console.log('request ', request.url);
 
@@ -53,5 +62,5 @@ http.createServer(function (request, response) {
     }
   });
 
-}).listen(8080);
-console.log('Server running at http://127.0.0.1:8080/');
+}).listen(port);
+console.log('Server running at http://127.0.0.1:'+ port);
